7ab10df866aa3b67f0a0b149d9b554980ba88c0b
ne006 <IATikhomirov@gmail.com>
2019-08-26 22:55:20 +0300

    Updated Gemfile, regened Webpack binstubs, fixed FactoryBot factories error

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,29 +1,29 @@
 FactoryBot.define do
 	factory :empty_hypothesis, class: Hypothesis do
-		dataset []
+		initialize_with { new(dataset: []) }
 	end
 
 	factory :nil_hypothesis, class: Hypothesis do
-		dataset nil
+		initialize_with { new(dataset: nil) }
 	end
 
 	factory :invalid_hypothesis, class: Hypothesis do
-		dataset "qwerty"
+		initialize_with { new(dataset: "qwerty") }
 	end
 
 	factory :invalid_point_hypothesis, class: Hypothesis do
-		dataset [{x: 5}, {y: 6}, {z: 1}, {}]
+		initialize_with { new(dataset: [{x: 5}, {y: 6}, {z: 1}, {}]) }
 	end
 
 	factory :one_point_hypothesis, class: Hypothesis do
-		dataset [{x: 1, y: 1}]
+		initialize_with { new(dataset: [{x: 1, y: 1}]) }
 	end
 
 	factory :two_point_hypothesis, class: Hypothesis do
-		dataset [{x: 1, y: 1}, {x: 2, y: 2}]
+		initialize_with { new(dataset: [{x: 1, y: 1}, {x: 2, y: 2}]) }
 	end
 
 	factory :three_point_hypothesis, class: Hypothesis do
-		dataset [{x: 1, y: 1}, {x: 2, y: 2}, {x: 3, y: 3}]
+		initialize_with { new(dataset: [{x: 1, y: 1}, {x: 2, y: 2}, {x: 3, y: 3}]) }
 	end
 end
